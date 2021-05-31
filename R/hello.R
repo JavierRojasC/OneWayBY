72ba858e5cbdf371e7815e4b0dd8fdc5bb868c1d
JavierRojasC <62660573+JavierRojasC@users.noreply.github.com>
2021-05-31 16:50:58 -0500

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -9,3 +9,16 @@ example(source)
 try(roxygen2::roxygenize(load_code = "/Users/javierrojas/Documents/GitHub/OneWayBY/R/Example.R"), silent = TRUE)
 pkgbuild::compile_dll()
 roxygen2::roxygenize()
+rstan_create_package()
+
+rstan_create_package(
+  "/Users/javierrojas/Documents/GitHub/AnovaBYow",
+  fields = NULL,
+  rstudio = TRUE,
+  open = TRUE,
+  stan_files = character(),
+  roxygen = TRUE,
+  travis = TRUE,
+  license = TRUE,
+  auto_config = TRUE
+)
